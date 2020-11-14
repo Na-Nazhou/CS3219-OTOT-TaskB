018d60cd7a409866c874ecf8144bab9d467f532e
Na Nazhou <nazhou.na9@gmail.com>
2020-11-14 19:00:02 +0800

    Add CORS

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import serverless from 'serverless-http';
+import cors from 'cors';
 
 import router from './routes/index';
 
 export const app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
